82864dd3b9dd665d65c608ccc4c6510e06c15cb9
Victor Matveyshin <vic_m@ukr.net>
2026-04-20 23:34:59 +0300

    os path change

--- app/main.py
+++ app/main.py
@@ -11,23 +11,26 @@ def move_file(command: str) -> str:
 
     source_file = arguments[1]
     destination_dir = arguments[2].split("/")
-    destination_file = destination_dir.pop()
+    if arguments[2][-1] != "/":
+        destination_file = destination_dir.pop()
+    else:
+        destination_file = source_file
+
     common_dir = ""
 
     if len(destination_dir) != 0:
         for directory in destination_dir:
-            common_dir += directory
+            common_dir = os.path.join(common_dir, directory)
             try:
                 os.mkdir(common_dir)
                 pass
             except FileExistsError:
                 pass
-            finally:
-                common_dir += "/"
 
+    common_dir = os.path.join(common_dir, destination_file)
     try:
         with (open(source_file, "r") as file_in,
-              open(common_dir + destination_file, "w") as file_out):
+              open(common_dir, "w") as file_out):
             file_out.write(file_in.read())
     except FileNotFoundError as e:
         return str(e)
@@ -36,4 +39,6 @@ def move_file(command: str) -> str:
 
     os.remove(source_file)
     return (f"File {source_file} has been moved to "
-            f"{common_dir + destination_file}")
+            f"{common_dir}")
+
+move_file("mv file.txt first_dir/second_dir/third_dir/file2/ ")
